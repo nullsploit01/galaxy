5ceb48df7c71326496f345cbc046dd9ccfcca123
nullsploit01 <harshaldharmik099@gmail.com>
2025-04-12 19:23:29 +0530

    render multiple stars

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -1,40 +1,54 @@
 import { useLoader } from '@react-three/fiber';
-import { useMemo } from 'react';
+import { Fragment, useMemo } from 'react';
 import { AdditiveBlending, BufferAttribute, TextureLoader } from 'three';
 
 const Stars = () => {
-  const starTexture = useLoader(TextureLoader, './textures/star/star.png');
-  const count = 500000;
+  const textures = useLoader(
+    TextureLoader,
+    Array.from({ length: 10 }, (_, i) => `./textures/star/star${i + 1}.webp`),
+  );
+
+  const countPerLayer = 50000;
   const distance = 500;
-  const points = useMemo(() => {
-    const positions = new Float32Array(count * 3);
 
-    for (let i = 0; i < count; i++) {
-      const i3 = i * 3;
+  // Generate 10 star layers (one per texture)
+  const stars = useMemo(() => {
+    return textures.map((texture, index) => {
+      const positions = new Float32Array(countPerLayer * 3);
+      for (let i = 0; i < countPerLayer; i++) {
+        const i3 = i * 3;
+        positions[i3 + 0] = (Math.random() - 0.5) * distance;
+        positions[i3 + 1] = (Math.random() - 0.5) * distance;
+        positions[i3 + 2] = (Math.random() - 0.5) * distance;
+      }
 
-      positions[i3 + 0] = (Math.random() - 0.5) * distance;
-      positions[i3 + 1] = (Math.random() - 0.5) * distance;
-      positions[i3 + 2] = (Math.random() - 0.5) * distance;
-    }
-
-    return new BufferAttribute(positions, 3);
-  }, []);
+      return {
+        key: `star-layer-${index}`,
+        texture,
+        bufferAttribute: new BufferAttribute(positions, 3),
+      };
+    });
+  }, [textures]);
 
   return (
-    <points>
-      <bufferGeometry>
-        <bufferAttribute attach="attributes-position" args={[points.array, 3]} />
-      </bufferGeometry>
-      <pointsMaterial
-        map={starTexture}
-        size={1}
-        sizeAttenuation={true}
-        depthWrite={true}
-        transparent
-        alphaTest={0.5}
-        blending={AdditiveBlending}
-      />
-    </points>
+    <Fragment>
+      {stars.map(({ key, texture, bufferAttribute }) => (
+        <points key={key}>
+          <bufferGeometry>
+            <bufferAttribute attach="attributes-position" args={[bufferAttribute.array, 3]} />
+          </bufferGeometry>
+          <pointsMaterial
+            map={texture}
+            size={1}
+            sizeAttenuation
+            depthWrite={false}
+            transparent
+            alphaTest={0.5}
+            blending={AdditiveBlending}
+          />
+        </points>
+      ))}
+    </Fragment>
   );
 };
 
